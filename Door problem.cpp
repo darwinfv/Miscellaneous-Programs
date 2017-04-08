9862d120543766ff9d4d9e62859841e48e202230
Darwin Vaz <dvaz@purdue.edu>
2017-04-08 10:14:29 -0400

    Door problem

---
+++ Door problem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	
+	int totalDoors;
+	
+	cout<<"\nEnter the total number of doors: ";
+	cin>>totalDoors;
+	
+	int door[totalDoors]; //0 means closed, 1 means open
+	for(int i = 0; i < totalDoors; i++)
+		door[i] = 0;
+	
+	for(int i = 0; i < totalDoors; i++) {
+		for(int j = i; j < totalDoors; j++) {
+			
+		}
+	}
+				
+	getch();
+		
+}
