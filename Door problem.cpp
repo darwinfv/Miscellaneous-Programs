78e4b1cfc0993fd5a5d127b95de31e0a289ed549
Darwin Vaz <dvaz@purdue.edu>
2017-04-12 11:08:42 -0400

    Worked on door problem

--- Door problem.cpp
+++ Door problem.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 int main() {
 	
 	int totalDoors;
 	
-	cout<<"\nEnter the total number of doors: ";
+	cout<<"Enter the total number of doors: ";
 	cin>>totalDoors;
 	
 	int door[totalDoors]; //0 means closed, 1 means open
@@ -14,11 +13,29 @@ int main() {
 		door[i] = 0;
 	
 	for(int i = 0; i < totalDoors; i++) {
-		for(int j = i; j < totalDoors; j++) {
-			
+		for(int j = i; j < totalDoors; j+=(i+1)) {
+			if(door[j] == 0)
+				door[j]++;
+			else
+				door[j]--;
 		}
 	}
-				
-	getch();
-		
+	
+label:
+	
+	cout<<"\n1. View all doors\n2. View open doors\n3. View closed doors\n4. Try with a different number\n5. Exit\n\nSelect your choice: ";
+	int choice;
+	cin>>choice;
+	switch(choice) {
+		case 1:
+			
+		case 4:
+			clrscr();
+			main();
+		case 5:
+			exit(0);
+		default:
+			goto label;
+	}
+
 }
