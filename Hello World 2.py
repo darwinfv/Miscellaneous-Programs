613d0260037790de271f08f620d68941be26c2d5
Darwin Vaz <dvaz@purdue.edu>
2017-09-16 22:13:25 -0400

    Create Hello World 2.py

---
+++ Hello World 2.py
@@ -0,0 +1,62 @@
+# Version 3.6.2
+
+print("Hello World")
+
+num = 5
+print(num)
+
+myStr = "My String"
+print(myStr)
+print("This is" + myStr)
+
+print(6 + num)
+
+print(str(num) + "666")
+
+
+
+
+if(num < 10):
+  print(num < 10)
+elif(num > 9):
+  print("^^^")
+else:
+  print("")
+
+while True:
+  print("Infinite loop")
+
+while(num < 8):
+  count += 1
+
+myList = ["this", "something", "nothing", "everything"]
+
+for element in range(5):
+  print(element)
+
+for element in myList:
+  print(element)
+
+
+
+
+# functions
+
+def myFunction():
+  print("Inside ( ͡° ͜ʖ ͡°)")
+
+myFuncton()
+
+def Caller(x):
+  print(x+x)
+
+myFunction(-1)
+
+def sum(first, second):
+  return first + second
+
+print(sum(15, 16))
+
+
+
+
