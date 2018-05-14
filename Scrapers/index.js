706e91acf5faab153f83a6c110236becec1e64b5
Darwin Vaz <dvaz@purdue.edu>
2018-05-14 16:36:04 -0400

    Html to text, need DL

--- Scrapers/index.js
+++ Scrapers/index.js
@@ -1,19 +1,13 @@
 
-	const rp = require('request-promise');
-	const cheerio = require('cheerio');
+	let axios = require('axios');
+	let cheerio = require('cheerio');
+	let fs = require('fs');
 
-	const options = {
-	  uri: `http://www.math.purdue.edu/~li2285/courses/453f/ma453.html`,
-	  transform: function (body) {
-	    return cheerio.load(body);
-	  }
-	};
-
-	rp(options)
-	  .then(($('html')).html() => {
-	    console.log($);
-	  })
-	  .catch((err) => {
-	    console.log("Error");
-	  });
-
+	axios.get('http://www.math.purdue.edu/~li2285/courses/453f/ma453.html')
+	    .then((response) => {
+	        if(response.status === 200) {
+	        const html = response.data;
+	        console.log(cheerio.load(response.data).text());
+	            const $ = cheerio.load(html); 
+	    }
+	    }, (error) => console.log(err) );
