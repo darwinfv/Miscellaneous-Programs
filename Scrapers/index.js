e45321e5d0ac59b61246f986ab98f40057249f2a
Darwin Vaz <dvaz@purdue.edu>
2018-05-14 17:41:24 -0400

    Downloaded homepage

--- Scrapers/index.js
+++ Scrapers/index.js
@@ -6,8 +6,30 @@
 	axios.get('http://www.math.purdue.edu/~li2285/courses/453f/ma453.html')
 	    .then((response) => {
 	        if(response.status === 200) {
-	        const html = response.data;
-	        console.log(cheerio.load(response.data).text());
-	            const $ = cheerio.load(html); 
-	    }
+	        	const html = response.data;
+	            const $ = cheerio.load(html);
+	            fs.writeFile('index.html', response.data, (err) => console.log('File successfully written!'));
+	    	}
 	    }, (error) => console.log(err) );
+
+	/*axios.get('http://www.math.purdue.edu/~li2285/courses/453f/ma453.html')
+	    .then((response) => {
+	        if(response.status === 200) {
+	            const html = response.data;
+	            const $ = cheerio.load(html); 
+	            let devtoList = [];
+	            $('.single-article').each(function(i, elem) {
+	                devtoList[i] = {
+	                    title: "$(this).find('h3').text().trim(),",
+	                    url: $(this).children('.index-article-link').attr('href'),
+	                    tags: $(this).find('.tags').text().split('#')
+	                          .map(tag =>tag.trim())
+	                          .filter(function(n){ return n != "" })
+	                }      
+	            });
+	            const devtoListTrimmed = devtoList.filter(n => n != undefined )
+	            fs.writeFile('devtoList.json', 
+	                          JSON.stringify(devtoListTrimmed, null, 4), 
+	                          (err)=> console.log('File successfully written!'))
+	    }
+	}, (error) => console.log(err) );*/
