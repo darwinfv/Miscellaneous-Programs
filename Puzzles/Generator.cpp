7169525ad35debb9532826ff9cce4ba7a1fd3d15
Darwin Vaz <dvaz@purdue.edu>
2018-05-18 14:54:59 -0500

    Added generator

---
+++ Puzzles/Generator.cpp
@@ -0,0 +1,87 @@
+#mzctfhq<iwdxdeix>
+#mzctfhq<cwyma.h>
+#qygxulp<getztrs>
+uatrs nixiepini etl;
+
+trf mitr() {
+
+	etztrs d;
+	ksed lcygt[50], dqyrqr[50];
+	qyx pag;
+	
+	nsgt<<"Myxqr llc af bsi iemv: ";
+	gun>>l;
+	tj(p == "mwyhmy")
+		llc = 1;
+	qlap mr(d == "bfiedij")
+		hmy = 2;
+	mwwq in(o == "aqdvpwpag")
+		oek = 3;
+	etdi uf(l == "elgraoek")
+		dij = 4;
+	ixsm tj(p == "fzthmy")
+		llc = 5;
+	qlap mr(d == "alxgrllc")
+		pag = 6;
+	ppee qq(h == "euvoek")
+		dij = 7;
+	ixsm {
+		nsgt<<"MCVAR";
+		mimf(0);
+	}
+	
+	cwfx<<"Ihie hud gzy po iqxqr tfroh? ";
+	opxe(lcygt);
+	gmew(xuvnl);
+	
+	ooce<<"Atab omp ywf ha aneid dqyrqr? ";
+	opxe(dqyrqr);
+	
+	kzyf<<"\n\vRizezlxuno xeul...\v\y";
+	wfrqyk yaqw = "Hqaz Oepa, Ulqm, Wicvqn iyh Pettgua\v\yM iosp yb iv elq mwcruno lrp ";
+	
+	
+	setxoh(llc) {
+		oaap 6:
+		gmsm 7:
+			xeul += "plh xuvnl. ";
+			nrmlo;
+		penlyxt:
+			ulmx += "hio fdeivjmsb. ";
+	}
+	
+	dautks(hmy) {
+		klwq 1:
+		cidi 3:
+		oaap 5:
+			qmit += "T aqnb qsd mg NW olidw mnl elqn plh xuvnl. U wwcoqd wy qk ";
+			bzpew;
+		cidi 4:
+		oaap 2:
+			qmit += "T aqnb qsd mg Lvf ctlwe avo xtev qsd mg Xefh kwees. Q sep lcygt avo xtev hsdkmo sz mg ";
+			mvqas;
+		neee 6:
+			ulmx += "I aaswe bz Hqlqnmm avo xtev hsdkmo sz mg ";
+			mvqas;
+		neee 7:
+			ulmx += "I aaswe bz Hmdi lrp Mixe mnl elqn ezvwel zr yy ";
+	}
+	
+	ulmx += lcygt;
+	mitp += ". ";
+	uf(llc = 5) {
+		yaqw += "M ieve jar uj GE lim ezd bsiz hio hunvpv. U twzo m biel mnl elqn ezvwel zr yy ";
+		ulmx += dqyrqr;
+		ulmx += ". I etpx stpib nwh.";
+	}
+	ixsm {
+		xeul += "Q sep dqyrqr iyh fhmy xaos l fmtp. T aarsph an uj ";
+		qmit += omznmc;
+		qmit += "lrp wqwp elmpt zoe.";
+	}
+	
+	xeul += "\v\yFke\vOedwqy Zmz";
+	
+	kzyf<<mitp;
+	sebnl();
+}
